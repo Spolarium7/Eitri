483f880d78d96b00a12cae6f2b193055d80c6ee7
Goshen Jimenez <GoshenJimenez_BlueSkyPh>
2018-07-18 22:15:09 +0800

    Assign port number in Program.cs

--- GoshenJimenez.Eitri/GoshenJimenez.Eitri/Program.cs
+++ GoshenJimenez.Eitri/GoshenJimenez.Eitri/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -19,6 +20,7 @@ namespace GoshenJimenez.Eitri
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => { options.Listen(IPAddress.Loopback, 5020);})
                 .UseStartup<Startup>()
                 .Build();
     }
